7ed585c29e6f7a7f79f970b1912eb74e690e9888
yangkui <752544765@qq.com>
2021-10-05 18:00:03 +0800

    format shell script

--- jlink-script/start.sh
+++ jlink-script/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 MODULE_PATH="bin:lib"
 LIB_PATH="bin/*:lib/*"
-runtime/bin/java -cp ${LIB_PATH} --module-path ${MODULE_PATH} --add-modules ALL-MODULE-PATH -m cn.navclub.xtm.app
+runtime/bin/java \
+-cp ${LIB_PATH} \
+--module-path ${MODULE_PATH} \
+--add-modules ALL-MODULE-PATH -m cn.navclub.xtm.app
